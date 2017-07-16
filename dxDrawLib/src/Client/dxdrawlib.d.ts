0072f8a361badf94b6c9545237796aa3b8a7fbc5
SyBozz <kingsizee.de@gmail.com>
2017-07-16 03:15:40 +0200

    Change Colour => Color

--- dxDrawLib/src/Client/dxdrawlib.d.ts
+++ dxDrawLib/src/Client/dxdrawlib.d.ts
@@ -3,3 +3,5 @@
 // ELEMENTS
 /// <reference path="dxdrawlib/Elements/DxWindow.ts" />
 /// <reference path="dxdrawlib/Elements/DxElement.ts" />
+/// <reference path="dxdrawlib/Helpers/Color.ts" />
+/// <reference path="dxdrawlib/Helpers/DxScreen.ts" />
--- dxDrawLib/src/Client/dxdrawlib/helpers/Color.ts
+++ dxDrawLib/src/Client/dxdrawlib/helpers/Color.ts
@@ -1,6 +1,6 @@
 ﻿/// <reference path="../../dxdrawlib.d.ts" />
 
-class Colour {
+class Color {
 
 	constructor(public r: number, public g: number, public b: number, public a: number) {
 		this.r = (this.r > 255) ? 255 : ((this.r < 0) ? 0 : this.r);
