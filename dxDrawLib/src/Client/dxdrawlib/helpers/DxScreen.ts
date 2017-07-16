1450db1aa209d349334e15a5a1763c59ac7e876c
SyBozz <kingsizee.de@gmail.com>
2017-07-16 03:05:58 +0200

    Created Colour, DxScreen, DxElement

---
+++ dxDrawLib/src/Client/dxdrawlib/helpers/DxScreen.ts
@@ -0,0 +1,13 @@
+﻿/// <reference path="../../dxdrawlib.d.ts" />
+
+class DxScreen {
+	static width: number;
+	static height: number;
+}
+
+API.onResourceStart.connect(
+	function () {
+		DxScreen.width = API.getScreenResolution().Width;
+		DxScreen.height = API.getScreenResolution().Height;
+	}
+);
