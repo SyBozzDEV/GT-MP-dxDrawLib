ec7c3f81f72ebf2a32e2137e3a0472c9376de605
SyBozz <kingsizee.de@gmail.com>
2017-07-16 04:25:40 +0200

    Add constructor for DxElements, color

--- dxDrawLib/src/Client/dxdrawlib.d.ts
+++ dxDrawLib/src/Client/dxdrawlib.d.ts
@@ -3,5 +3,6 @@
 // ELEMENTS
 /// <reference path="dxdrawlib/Elements/DxWindow.ts" />
 /// <reference path="dxdrawlib/Elements/DxElement.ts" />
+
 /// <reference path="dxdrawlib/Helpers/Color.ts" />
 /// <reference path="dxdrawlib/Helpers/DxScreen.ts" />
---
+++ dxDrawLib/src/Client/dxdrawlib/Elements/DElement.ts
@@ -0,0 +1,22 @@
+﻿/// <reference path="../../dxdrawlib.d.ts" />
+
+class DxElement {
+
+	constructor(public X: number, public Y: number, public width: number, public height: number, public relative: boolean = true, public color?: Color) {
+
+		if (this.relative) {
+			this.X = DxScreen.width / this.X;
+			this.Y = DxScreen.height / this.Y;
+			this.width = DxScreen.width / this.width;
+			this.height = DxScreen.height / this.height;
+		}
+
+		this.X = (this.X > DxScreen.width) ? DxScreen.width : ((this.X < 0) ? 0 : this.X);
+		this.Y = (this.Y > DxScreen.height) ? DxScreen.height : ((this.Y < 0) ? 0 : this.Y);
+		this.width = (this.width > DxScreen.width) ? DxScreen.width : ((this.width < 0) ? 0 : this.width);
+		this.height = (this.height > DxScreen.height) ? DxScreen.height : ((this.height < 0) ? 0 : this.height);
+
+		if (this.color == null) this.color = new Color();
+	}
+
+}
